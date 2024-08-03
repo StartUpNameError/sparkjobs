53387f950833e1d4d4ad00a7f10ad701e72f3750
Ramón Amézquita <rameviz@icloud.com>
2024-08-03 11:15:40 -0700

    replacing click with argparse

--- src/main.py
+++ src/main.py
@@ -1,68 +1,64 @@
+import argparse
 import importlib
-import time
 
-import click
 from pyspark.sql import SparkSession
 
+_CLI_DOCSTRING = """
+Entrypoint for running PySpark jobs.
 
-def args_to_dict(arguments: list[str]) -> dict[str, str]:
-    """Converts user args to a dictionary.
+Job arguments are given using the `--job-args` option as
+a list of space-separated key=value pairs. Example:
 
-    Parameters
-    ----------
-    arguments : list of str
-        User arguments.
-
-    Returns
-    -------
-    dict : dict, str -> str
-        User args parsed into a dictionary of key:value pairs.
-    """
-    user_dict: dict[str, str] = {}
-
-    for arg in arguments:
-        split = arg.split("=", maxsplit=1)
-
-        if len(split) != 2:
-            raise ValueError(
-                f"Invalid format for argument '{arg}'. Use -A name=value."
-            )
-
-        name, value = split
-        if name in user_dict:
-            raise ValueError(f"Repeated parameter: '{name}'")
-
-        user_dict[name] = value
-    return user_dict
+    --job-args key1=value1 key2=value2 key3=value3
+"""
 
 
-@click.command()
-@click.option(
-    "--job",
-    "-J",
-    required=True,
-    type=str,
-    help="The name of the job module you want to run.",
-)
-@click.option(
-    "--args-list",
-    "-A",
-    metavar="NAME=VALUE",
-    multiple=True,
-    help="An argument for the run, of the form -A name=value.",
-)
-def main(job, args_list):
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=_CLI_DOCSTRING, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument(
+        "--job",
+        type=str,
+        dest="job_name",
+        help="<Required> Name of the job to run.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--job-args",
+        nargs="*",
+        help="List of space-separated key=value pairs.",
+    )
+
+    return parser
+
+
+def resolve_args(args: list[str] | None = None) -> dict:
+
+    if args is None:
+        return {}
+
+    job_args_tuples = [arg_str.split("=") for arg_str in args]
+    return {a[0]: a[1] for a in job_args_tuples}
+
+
+def main():
     """Runs a PySpark job."""
 
-    args_dict = args_to_dict(args_list)
-    spark = SparkSession.builder.appName(job).getOrCreate()
-    job_module = importlib.import_module(f"jobs.{job}")
+    parser = create_parser()
+    parsed_args, unknown = parser.parse_known_args()
 
-    start = time.time()
-    job_module.run(spark=spark, **args_dict)
-    end = time.time()
+    # Resolve user args.
+    job_name = parsed_args.job_name
+    job_args = resolve_args(args=parsed_args.job_args)
+    print(f"Called job: {job_name} with arguments: {job_args}")
 
-    print(f"Execution of job {job} took {end - start} seconds.")
+    spark = SparkSession.builder.appName(f"{job_name}").getOrCreate()
+
+    job_module = importlib.import_module("jobs.{job_name}")
+    job_module.run(spark, **job_args)
 
 
 if __name__ == "__main__":
